036d595e34b244a7e6a7244dda661fdf69f4e00d
Rahul <rahulganesamani@gmail.com>
2023-05-24 16:04:15 +0530

    Code 3 : String input

---
+++ code_3_string_input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Hey, may I know your name?" << endl;
+    cout << "Hello, I am ";
+    cin >> name;
+    cout << "Good to see you, " << name << endl;
+    return 0;
+}
