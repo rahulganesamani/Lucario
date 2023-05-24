bfc4f16bc2d9f0cf4fda22777c4adb3d3e1528f2
Rahul <rahulganesamani@gmail.com>
2023-05-24 15:58:52 +0530

    Code 2 : Add two integers

---
+++ code_2_add_two_integers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter two numbers a and b : ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "The sum of the given numbers is " << c << endl;
+    return 0;
+}
